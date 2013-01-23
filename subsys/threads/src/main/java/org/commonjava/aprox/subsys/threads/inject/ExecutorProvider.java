e72e6c585c53ff1168a619da9beacff9d87fd750
John Casey <jdcasey@commonjava.org>
2013-01-22 22:14:03 -0600

    destroy the executors the provider starts.

--- subsys/threads/src/main/java/org/commonjava/aprox/subsys/threads/inject/ExecutorProvider.java
+++ subsys/threads/src/main/java/org/commonjava/aprox/subsys/threads/inject/ExecutorProvider.java
@@ -1,20 +1,54 @@
 package org.commonjava.aprox.subsys.threads.inject;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.commonjava.util.logging.Logger;
 
 @ApplicationScoped
 public class ExecutorProvider
 {
 
     private final Map<String, ExecutorService> services = new HashMap<String, ExecutorService>();
+
+    private final Logger logger = new Logger( getClass() );
+
+    @PreDestroy
+    public void shutdown()
+    {
+        for ( final Map.Entry<String, ExecutorService> entry : services.entrySet() )
+        {
+            final ExecutorService service = entry.getValue();
+
+            service.shutdown();
+            try
+            {
+                service.awaitTermination( 1000, TimeUnit.MILLISECONDS );
+
+                if ( !service.isTerminated() )
+                {
+                    final List<Runnable> running = service.shutdownNow();
+                    if ( !running.isEmpty() )
+                    {
+                        logger.warn( "%d tasks remain for executor: %s", running.size(), entry.getKey() );
+                    }
+                }
+            }
+            catch ( final InterruptedException e )
+            {
+            }
+        }
+    }
 
     @Produces
     public ExecutorService getExecutorService( final InjectionPoint ip )
