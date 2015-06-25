a10c64cee162af0905c0f75961c3b960021425c6
John Casey <jdcasey@commonjava.org>
2015-06-25 16:36:12 -0500

    reformat for license header, and adding blacklist capability to implied-repos. Also, exclude swagger ui from license formatting

--- addons/implied-repos/common/src/main/java/org/commonjava/aprox/implrepo/conf/ImpliedRepoConfig.java
+++ addons/implied-repos/common/src/main/java/org/commonjava/aprox/implrepo/conf/ImpliedRepoConfig.java
@@ -17,51 +17,38 @@ package org.commonjava.aprox.implrepo.conf;
 
 import java.io.File;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Alternative;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.commonjava.aprox.conf.AbstractAproxConfigInfo;
 import org.commonjava.aprox.conf.AbstractAproxFeatureConfig;
+import org.commonjava.aprox.conf.AbstractAproxMapConfig;
 import org.commonjava.aprox.conf.AproxConfigClassInfo;
 import org.commonjava.aprox.conf.AproxConfigInfo;
 import org.commonjava.web.config.ConfigurationException;
-import org.commonjava.web.config.annotation.ConfigName;
 import org.commonjava.web.config.annotation.SectionName;
 
-@SectionName( "implied-repos" )
+@SectionName( ImpliedRepoConfig.SECTION_NAME )
 @Alternative
 @Named
 public class ImpliedRepoConfig
+    extends AbstractAproxMapConfig
+    implements AproxConfigClassInfo
 {
 
-    @javax.enterprise.context.ApplicationScoped
-    public static class ConfigInfo
-        extends AbstractAproxConfigInfo
-    {
-        public ConfigInfo()
-        {
-            super( ImpliedRepoConfig.class );
-        }
+    public static final String SECTION_NAME = "implied-repos";
 
-        @Override
-        public String getDefaultConfigFileName()
-        {
-            return new File( AproxConfigInfo.CONF_INCLUDES_DIR, "implied-repos.conf" ).getPath();
-        }
+    public static final String ENABLED_KEY = "enabled";
 
-        @Override
-        public InputStream getDefaultConfig()
-        {
-            return Thread.currentThread()
-                         .getContextClassLoader()
-                         .getResourceAsStream( "default-implied-repos.conf" );
-        }
-    }
+    public static final String INCLUDE_SNAPSHOTS_KEY = "include.snapshots";
+
+    public static final String DISABLED_HOST_KEY = "disable";
 
     public static final boolean DEFAULT_ENABLED = false;
 
@@ -70,6 +57,8 @@ public class ImpliedRepoConfig
     private Boolean enabled;
 
     private Boolean includeSnapshotRepos;
+
+    private List<String> blacklist = new ArrayList<>();
 
     public ImpliedRepoConfig()
     {
@@ -80,7 +69,6 @@ public class ImpliedRepoConfig
         return enabled == null ? DEFAULT_ENABLED : enabled;
     }
 
-    @ConfigName( "enabled" )
     public void setEnabled( final Boolean enabled )
     {
         this.enabled = enabled;
@@ -91,38 +79,113 @@ public class ImpliedRepoConfig
         return includeSnapshotRepos == null ? DEFAULT_INCLUDE_SNAPSHOT_REPOS : includeSnapshotRepos;
     }
 
-    @ConfigName( "include.snapshots" )
     public void setIncludeSnapshotRepos( final Boolean includeSnapshotRepos )
     {
         this.includeSnapshotRepos = includeSnapshotRepos;
+    }
+    
+    public void addBlacklist( final String host )
+    {
+        this.blacklist.add( host );
+    }
+
+    public boolean isBlacklisted( final URL url )
+    {
+        final String proto = url.getProtocol();
+        final String host = url.getHost();
+        int port = url.getPort();
+        if ( port < 0 )
+        {
+            port = "https".equals( proto ) ? 443 : 80;
+        }
+
+        return blacklist.contains( host ) || blacklist.contains( host + ":" + port )
+            || blacklist.contains( proto + "://" + host ) || blacklist.contains( proto + "://" + host + ":" + port );
+    }
+
+    public List<String> getBlacklist()
+    {
+        return blacklist;
+    }
+
+    public void setBlacklist( final List<String> blacklist )
+    {
+        this.blacklist = blacklist;
+    }
+
+    @Override
+    public void parameter( final String name, final String value )
+        throws ConfigurationException
+    {
+        switch ( name )
+        {
+            case ENABLED_KEY:
+            {
+                this.enabled = Boolean.parseBoolean( value );
+                break;
+            }
+            case INCLUDE_SNAPSHOTS_KEY:
+            {
+                this.includeSnapshotRepos = Boolean.parseBoolean( value );
+                break;
+            }
+            case DISABLED_HOST_KEY:
+            {
+                this.blacklist.add( value );
+                break;
+            }
+            default:
+            {
+                throw new ConfigurationException(
+                                                  "Invalid value: '{}' for parameter: '{}'. Only numeric values are accepted for section: '{}'.",
+                                                  value, name, SECTION_NAME );
+            }
+        }
     }
 
     @javax.enterprise.context.ApplicationScoped
     public static class FeatureConfig
         extends AbstractAproxFeatureConfig<ImpliedRepoConfig, ImpliedRepoConfig>
     {
-        @Inject
-        private ConfigInfo info;
-
         public FeatureConfig()
         {
-            super( ImpliedRepoConfig.class );
+            super( new ImpliedRepoConfig() );
         }
 
         @Produces
         @Default
         @ApplicationScoped
-        public ImpliedRepoConfig getFlatFileConfig()
+        public ImpliedRepoConfig getImpliedRepoConfig()
             throws ConfigurationException
         {
-            return getConfig();
+            return getPrefabInstance();
         }
 
         @Override
         public AproxConfigClassInfo getInfo()
         {
-            return info;
+            return getPrefabInstance();
         }
+    }
+
+    @Override
+    public String getDefaultConfigFileName()
+    {
+        return new File( AproxConfigInfo.CONF_INCLUDES_DIR, "implied-repos.conf" ).getPath();
+    }
+
+    @Override
+    public InputStream getDefaultConfig()
+    {
+        return Thread.currentThread()
+                     .getContextClassLoader()
+                     .getResourceAsStream( "default-implied-repos.conf" );
+    }
+
+    @Override
+    public Class<?> getConfigurationClass()
+    {
+        return getClass();
     }
 
 }
