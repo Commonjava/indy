6e16291a9a50e9a4e1269ecaaf450ec52a9b5abb
gli <gli@redhat.com>
2016-05-16 13:33:14 +0800

    change all ftests which use TestHttpServer to ExpectationServer because TestHttpServer is deprecated

--- ftests/core/src/main/java/org/commonjava/indy/ftest/core/content/ProxyRemoteContentTest.java
+++ ftests/core/src/main/java/org/commonjava/indy/ftest/core/content/ProxyRemoteContentTest.java
@@ -25,7 +25,6 @@ import java.io.InputStream;
 
 import org.commonjava.indy.client.core.helper.PathInfo;
 import org.commonjava.indy.model.core.RemoteRepository;
-import org.commonjava.test.http.TestHttpServer;
 import org.commonjava.test.http.expect.ExpectationServer;
 import org.junit.Rule;
 import org.junit.Test;
