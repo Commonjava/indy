81e5437a4cb2f602a6325058881381f1a9fc9aeb
John Casey <jdcasey@commonjava.org>
2016-05-17 15:55:51 -0500

    upgrade to releases of partyline and galley, add license headers to new tests, and enable GroupMetadataOverlappingWithoutMetadataOfHostedReposTest

--- ftests/core/src/main/java/org/commonjava/indy/ftest/core/content/GroupMetadataOverlappingWithoutMetadataOfHostedReposTest.java
+++ ftests/core/src/main/java/org/commonjava/indy/ftest/core/content/GroupMetadataOverlappingWithoutMetadataOfHostedReposTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2011 Red Hat, Inc. (jdcasey@commonjava.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.commonjava.indy.ftest.core.content;
 
 import org.apache.commons.io.IOUtils;
@@ -43,7 +58,6 @@ public class GroupMetadataOverlappingWithoutMetadataOfHostedReposTest
     public ExpectationServer server = new ExpectationServer();
 
     @Test
-    @Ignore
     public void run()
             throws Exception
     {
@@ -125,7 +139,8 @@ public class GroupMetadataOverlappingWithoutMetadataOfHostedReposTest
         HostedRepository hostedRepository = new HostedRepository( hostedRepo );
         hostedRepository = client.stores().create( hostedRepository, "adding hosted", HostedRepository.class );
 
-        final String normalPath = "org/foo/bar/1.0/bar-1.0.pom";
+        // don't use the same GA as above, in case we ever decide to "actively" manage metadata in the repo manager (hint: we will)
+        final String normalPath = "org/foo/other/1.0/other-1.0.pom";
         client.content()
               .store( hostedRepository.getKey(), normalPath,
                       new ByteArrayInputStream( "<version>1.0</version>".getBytes( "UTF-8" ) ) );
