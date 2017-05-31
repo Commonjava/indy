49207cedaead045968cace96a1b672636bc4c222
Petr Kocandrle <pkocandr@redhat.com>
2017-05-31 10:46:52 +0200

    Remove unreachable deletion
    The deletion because it is surrounded by:
    
    if ( ... || path.endsWith( LISTING_HTML_FILE ) ) {
        ...
    } else {
        ...
        if ( !item.exists() ) {
            ...
        } else if ( item.isDirectory() || ( path.endsWith( LISTING_HTML_FILE ) ) {
            if ( item.isFile() && item.getLocation().allowsDownloading() ) {
                item.delete( false );
            }
        }
    }
    
    The first if is activated in case path ends with the "index.html" and
    there is no additional condition, so it cannot be true in the else
    block. So on the path leading to the deletion there has to be true
    item.isDirectory(). but the deletion itself would also require
    item.isFile() and that cannot be true at that point.

--- bindings/jaxrs/src/main/java/org/commonjava/indy/core/bind/jaxrs/ContentAccessHandler.java
+++ bindings/jaxrs/src/main/java/org/commonjava/indy/core/bind/jaxrs/ContentAccessHandler.java
@@ -392,15 +392,10 @@ public class ContentAccessHandler
                     {
                         return handleMissingContentQuery( sk, path, builderModifier );
                     }
-                    else if ( item.isDirectory() || ( path.endsWith( LISTING_HTML_FILE ) ) )
+                    else if ( item.isDirectory() )
                     {
                         try
                         {
-                            if ( item.isFile() && item.getLocation().allowsDownloading() )
-                            {
-                                item.delete( false );
-                            }
-
                             logger.info( "Getting listing at: {}", path + "/" );
                             final String content =
                                     contentController.renderListing( standardAccept, st, name, path + "/", baseUri,
@@ -408,7 +403,7 @@ public class ContentAccessHandler
 
                             response = formatOkResponseWithEntity( content, acceptInfo.getRawAccept(), builderModifier );
                         }
-                        catch ( final IndyWorkflowException | IOException e )
+                        catch ( final IndyWorkflowException e )
                         {
                             logger.error(
                                     String.format( "Failed to render content listing: %s from: %s. Reason: %s", path,
