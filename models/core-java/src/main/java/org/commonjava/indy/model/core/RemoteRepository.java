b82601e97110b2914c743e60f74475584b09edcc
Gang Li <gli@redhat.com>
2018-05-10 16:59:49 +0800

    [Prefetch] model changes and ui changes

--- models/core-java/src/main/java/org/commonjava/indy/model/core/RemoteRepository.java
+++ models/core-java/src/main/java/org/commonjava/indy/model/core/RemoteRepository.java
@@ -38,6 +38,10 @@ public class RemoteRepository
 
     private static final Logger LOGGER = LoggerFactory.getLogger( RemoteRepository.class );
 
+    public static final String PREFETCH_LISTING_TYPE_KOJI = "koji";
+
+    public static final String PREFETCH_LISTING_TYPE_HTML = "html";
+
     @ApiModelProperty( required = true, value = "The remote URL to proxy" )
     @JsonProperty( "url" )
     private String url;
@@ -88,6 +92,19 @@ public class RemoteRepository
 
     @JsonProperty( "server_trust_policy" )
     private String serverTrustPolicy;
+
+    @ApiModelProperty(
+            value = "Integer to indicate the pre-fetching priority of the remote, higher means more eager to do the pre-fetching of the content in the repo, 0 or below means disable the pre-fecthing." )
+    @JsonProperty( "prefetch_priority" )
+    private Integer prefetchPriority = 0;
+
+    @ApiModelProperty( value = "Indicates if the remote needs to do rescan after prefetch" )
+    @JsonProperty( "prefetch_rescan" )
+    private boolean prefetchRescan = false;
+
+    @ApiModelProperty( value = "The prefetch listing type, should be html or koji" )
+    @JsonProperty( "prefetch_listing_type" )
+    private String prefetchListingType = PREFETCH_LISTING_TYPE_HTML;
 
     RemoteRepository()
     {
@@ -363,6 +380,36 @@ public class RemoteRepository
     public void setServerTrustPolicy( final String serverTrustPolicy )
     {
         this.serverTrustPolicy = serverTrustPolicy;
+    }
+
+    public Integer getPrefetchPriority()
+    {
+        return prefetchPriority;
+    }
+
+    public void setPrefetchPriority( Integer prefetchPriority )
+    {
+        this.prefetchPriority = prefetchPriority;
+    }
+
+    public boolean isPrefetchRescan()
+    {
+        return prefetchRescan;
+    }
+
+    public void setPrefetchRescan( boolean prefetchRescan )
+    {
+        this.prefetchRescan = prefetchRescan;
+    }
+
+    public String getPrefetchListingType()
+    {
+        return prefetchListingType;
+    }
+
+    public void setPrefetchListingType( String prefetchListingType )
+    {
+        this.prefetchListingType = prefetchListingType;
     }
 
     @Override
