2c472dbc4dd27a9eb610453ad31d30cea92d2d19
Gang Li <gli@redhat.com>
2018-08-23 17:46:42 +0800

    Add hosted by archive addon module
       Includes:
       * REST endpoint:
    admin/stores/maven/hosted/{repo}/compressed-content?ignoreBeforeRoot=xxx
       * java client module
       * Config to disable/enable
       * Some ftests

---
+++ addons/hosted-by-archive/common/src/main/java/org/commonjava/indy/hostedbyarc/HostedByArchiveManager.java
@@ -0,0 +1,312 @@
+/**
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.hostedbyarc;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.commonjava.cdi.util.weft.ExecutorConfig;
+import org.commonjava.cdi.util.weft.WeftManaged;
+import org.commonjava.indy.IndyWorkflowException;
+import org.commonjava.indy.audit.ChangeSummary;
+import org.commonjava.indy.content.ContentManager;
+import org.commonjava.indy.data.IndyDataException;
+import org.commonjava.indy.data.StoreDataManager;
+import org.commonjava.indy.hostedbyarc.config.HostedByArchiveConfig;
+import org.commonjava.indy.model.core.HostedRepository;
+import org.commonjava.indy.pkg.maven.model.MavenPackageTypeDescriptor;
+import org.commonjava.indy.util.ApplicationStatus;
+import org.commonjava.maven.galley.event.EventMetadata;
+import org.commonjava.maven.galley.model.TransferOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+@ApplicationScoped
+public class HostedByArchiveManager
+{
+    private final Logger logger = LoggerFactory.getLogger( this.getClass() );
+
+    public static final String TMP_DIR = StringUtils.defaultIfBlank( System.getProperty( "java.io.tmpdir" ), "/tmp" );
+
+    @Inject
+    private StoreDataManager storeDataManager;
+
+    @Inject
+    private ContentManager contentManager;
+
+    @Inject
+    private HostedByArchiveConfig config;
+
+    @Inject
+    @WeftManaged
+    @ExecutorConfig( named = "Hosted-by-arc-executor", priority = 1, threads = 8)
+    private ExecutorService executors;
+
+    public HostedRepository createStoreByArc( final byte[] archiveData, final String repoName, final String user,
+                                              final String pathPrefix, final boolean deleteFilesAfterUnzip )
+            throws IndyWorkflowException
+    {
+        final File archive;
+        try
+        {
+            archive = createTempArchiveFromUpload( archiveData );
+        }
+        catch ( IOException e )
+        {
+            throw new IndyWorkflowException( "Archive uploading failed!", e );
+        }
+
+        if ( !isZipFileType( archive ) )
+        {
+            throw new IndyWorkflowException( "Uploaded file is not a zip archive!" );
+        }
+
+        final String unzipPath = HostedByArchiveManager.TMP_DIR + "/" + System.currentTimeMillis();
+        final List<String> unzippedFiles;
+        try
+        {
+            unzippedFiles = unzip( archive, unzipPath );
+        }
+        catch ( IOException e )
+        {
+            throw new IndyWorkflowException( "Archive de-compressing failed!", e );
+        }
+
+        final HostedRepository repo = createHostedByName( repoName, user, "Create hosted by zip." );
+
+        storeUnzippedFiles( repo, unzippedFiles, unzipPath, pathPrefix );
+
+        try
+        {
+            if ( deleteFilesAfterUnzip )
+            {
+                Files.deleteIfExists( archive.toPath() );
+                deleteDirectory( new File( unzipPath ) );
+            }
+        }
+        catch ( IOException e )
+        {
+            logger.warn( "Delete unzipped files failed! Error: {}", e.getMessage() );
+        }
+
+        return repo;
+    }
+
+    private File createTempArchiveFromUpload( final byte[] archiveData )
+            throws IOException
+    {
+        final String fileName = "hosted" + System.currentTimeMillis() + ".zip";
+        File tempZipFile = new File( TMP_DIR + "/" + fileName );
+        if ( !tempZipFile.exists() )
+        {
+            tempZipFile.createNewFile();
+        }
+
+        try (FileOutputStream fop = new FileOutputStream( tempZipFile ))
+        {
+            IOUtils.write( archiveData, fop );
+        }
+
+        return tempZipFile;
+    }
+
+    private List<String> unzip( final File zipFile, final String unzipPath )
+            throws IOException
+    {
+        final List<String> filePaths = new ArrayList<>();
+        final File unzipDir = new File( unzipPath );
+        if ( unzipDir.exists() )
+        {
+            deleteDirectory( unzipDir );
+        }
+
+        if ( unzipDir.mkdirs() )
+        {
+            try (ZipInputStream zis = new ZipInputStream( new FileInputStream( zipFile ) ))
+            {
+                ZipEntry zipEntry = zis.getNextEntry();
+                while ( zipEntry != null )
+                {
+                    String fileName = zipEntry.getName();
+                    File newFile = new File( unzipPath + "/" + fileName );
+                    if ( zipEntry.isDirectory() )
+                    {
+                        newFile.mkdirs();
+                    }
+                    else
+                    {
+                        filePaths.add( fileName );
+                        if ( !newFile.getParentFile().exists() )
+                        {
+                            newFile.getParentFile().mkdirs();
+                        }
+                        try (FileOutputStream fos = new FileOutputStream( newFile ))
+                        {
+                            IOUtils.copy( zis, fos );
+                        }
+                    }
+                    zipEntry = zis.getNextEntry();
+                }
+                zis.closeEntry();
+            }
+        }
+
+        return filePaths;
+    }
+
+    private HostedRepository createHostedByName( final String repoName, final String user, final String changeLog )
+            throws IndyWorkflowException
+    {
+        final HostedRepository hosted = new HostedRepository( MavenPackageTypeDescriptor.MAVEN_PKG_KEY, repoName );
+        String storeChangeLog = changeLog;
+        if ( StringUtils.isBlank( changeLog ) )
+        {
+            storeChangeLog = "Changelog not provided";
+        }
+
+        final ChangeSummary summary = new ChangeSummary( user, storeChangeLog );
+
+        logger.trace( "Persisting hosted store: {} using: {}", hosted, storeDataManager );
+        try
+        {
+
+            if ( storeDataManager.storeArtifactStore( hosted, summary, true, true, new EventMetadata() ) )
+            {
+                return hosted;
+            }
+            else
+            {
+                throw new IndyWorkflowException( ApplicationStatus.SERVER_ERROR.code(), "Failed to store: {}. ",
+                                                 hosted.getKey() );
+            }
+        }
+        catch ( final IndyDataException e )
+        {
+            throw new IndyWorkflowException( ApplicationStatus.SERVER_ERROR.code(), "Failed to store: {}. Reason: {}",
+                                             e, hosted.getKey(), e.getMessage() );
+        }
+    }
+
+    private void storeUnzippedFiles( final HostedRepository repo, List<String> paths, final String unzippedDir,
+                                     final String pathPrefix ) throws IndyWorkflowException
+    {
+        final CountDownLatch latch = new CountDownLatch( paths.size() );
+
+        paths.forEach( s -> {
+            executors.execute( () -> {
+                String path = s.startsWith( "/" ) ? s : "/" + s;
+                if ( StringUtils.isNotBlank( pathPrefix ) )
+                {
+                    if ( path.startsWith( pathPrefix ) )
+                    {
+                        path = path.replaceFirst( pathPrefix, "" );
+                    }
+                }
+                File f = new File( unzippedDir + "/" + s );
+                try
+                {
+                    contentManager.store( repo, path, new FileInputStream( f ), TransferOperation.UPLOAD );
+                }
+                catch ( IndyWorkflowException | FileNotFoundException e )
+                {
+                    logger.error( "store failed for path {}", s, e );
+                }
+                finally
+                {
+                    latch.countDown();
+                }
+            } );
+        } );
+
+        try
+        {
+            latch.await( config.getLockTimeoutMins(), TimeUnit.MINUTES);
+        }
+        catch ( InterruptedException e )
+        {
+            throw new IndyWorkflowException("File store process interrupted!", e);
+        }
+    }
+
+    private void deleteDirectory( final File dir )
+            throws IOException
+    {
+        Files.list( dir.toPath() ).forEach( p -> {
+            try
+            {
+                if ( Files.isDirectory( p, LinkOption.NOFOLLOW_LINKS ) )
+                {
+                    deleteDirectory( p.toFile() );
+                }
+                else
+                {
+                    Files.delete( p );
+                }
+            }
+            catch ( IOException e )
+            {
+                logger.warn( "Delete failed for deleting directory {}!", dir );
+            }
+        } );
+        Files.deleteIfExists( dir.toPath() );
+    }
+
+    private boolean isZipFileType( final File file )
+    {
+        if ( file == null || !file.exists() )
+        {
+            return false;
+        }
+        try
+        {
+            return Files.probeContentType( file.toPath() ).contains( "application/zip" );
+        }
+        catch ( IOException e )
+        {
+            logger.warn( "zip file check failed. Failed message is {}", e.getMessage() );
+            return false;
+        }
+
+        // Another way
+        //        try
+        //        {
+        //            ZipFile zipFile = new ZipFile( file );
+        //            return true;
+        //        }
+        //        catch ( IOException e )
+        //        {
+        //            logger.warn("zip file check failed. Failed message is {}", e.getMessage());
+        //            return false
+        //        }
+
+    }
+}
---
+++ addons/hosted-by-archive/ftests/src/main/java/org/commonjava/indy/hostedbyarc/ftests/AbstractHostedByArcTest.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.hostedbyarc.ftests;
+
+import org.apache.commons.io.IOUtils;
+import org.commonjava.indy.client.core.IndyClientModule;
+import org.commonjava.indy.ftest.core.AbstractIndyFunctionalTest;
+import org.commonjava.indy.hostedbyarc.client.IndyHostedByArchiveClientModule;
+import org.commonjava.indy.test.fixture.core.CoreServerFixture;
+import org.junit.Before;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.Assert.fail;
+
+public abstract class AbstractHostedByArcTest
+        extends AbstractIndyFunctionalTest
+{
+    private IndyHostedByArchiveClientModule hostedByArchiveClientModule = new IndyHostedByArchiveClientModule();
+
+    private File zipFile;
+
+    @Before
+    public void prepare()
+            throws Exception
+    {
+        final String ZIP_FILE = getZipFileResource();
+        zipFile = getTemp().newFile( ZIP_FILE );
+
+        try (InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream( ZIP_FILE );
+             OutputStream zipFileOut = new FileOutputStream( zipFile ))
+        {
+            if ( stream == null )
+            {
+                fail( "Cannot find classpath test zip: " + ZIP_FILE );
+            }
+
+            IOUtils.copy( stream, zipFileOut );
+        }
+
+        if ( zipFile == null || !zipFile.exists() )
+        {
+            throw new IllegalStateException( String.format( "Zip file %s not found", ZIP_FILE ) );
+        }
+    }
+
+    @Override
+    protected Collection<IndyClientModule> getAdditionalClientModules()
+    {
+        return Collections.singletonList( hostedByArchiveClientModule );
+    }
+
+    @Override
+    protected void initTestConfig( CoreServerFixture fixture )
+            throws IOException
+    {
+        super.initTestConfig( fixture );
+        writeConfigFile( "conf.d/threadpools.conf", "[threadpools]\nenabled=true" );
+        if ( enabled() )
+        {
+            writeConfigFile( "conf.d/hosted-by-archive.conf", "[hosted-by-archive]\nenabled=true" );
+        }
+    }
+
+
+
+    protected File getZipFile()
+    {
+        return zipFile;
+    }
+
+    protected abstract String getZipFileResource();
+
+    protected abstract boolean enabled();
+
+
+}
