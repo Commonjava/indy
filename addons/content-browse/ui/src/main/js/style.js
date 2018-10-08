e662a007d2930c9faf913142f750851e1fd4c291
Gang Li <gli@redhat.com>
2018-10-08 17:44:34 +0800

    Init content-browse ui module and add draft ui resources

---
+++ addons/content-browse/ui/src/main/js/style.js
@@ -0,0 +1,15 @@
+const Header = {
+ color: "#333"
+}
+const ItemListing = {
+ listStyle: "none outside"
+}
+const Footer = {
+ borderTop: "1px solid #777",
+ fontSize: "small"
+}
+export const styles = {
+  Header: Header,
+  ItemListing: ItemListing,
+  Footer: Footer
+}
