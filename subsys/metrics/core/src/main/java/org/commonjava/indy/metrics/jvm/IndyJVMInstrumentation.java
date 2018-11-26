a2308ae6b3156c3613341fc157beb3650b01a1f5
Gang Li <gli@redhat.com>
2018-11-26 14:48:30 +0800

    Split metrics to core and reporter modules
     Current metrics module has dependency on some subsys modules like
    flatfile and db, which makes it is not possible to add metrics for these
    modules. So split metrics to core and reporter modules, which makes core
    to contain measure related apis like annotation and checker, and
    reporter contains reporter part of metrics like graphite and zabbix.

---
+++ subsys/metrics/core/src/main/java/org/commonjava/indy/metrics/jvm/IndyJVMInstrumentation.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) 2011-2018 Red Hat, Inc. (https://github.com/Commonjava/indy)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.metrics.jvm;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jvm.BufferPoolMetricSet;
+import com.codahale.metrics.jvm.ClassLoadingGaugeSet;
+import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
+
+import java.lang.management.ManagementFactory;
+
+import static com.codahale.metrics.MetricRegistry.name;
+
+/**
+ * Created by xiabai on 3/10/17.
+ */
+public class IndyJVMInstrumentation
+{
+    private static final String JVM_MEMORY = "jvm.memory";
+
+    private static final String JVM_GARBAGE = "jvm.garbage";
+
+    private static final String JVM_THREADS = "jvm.threads";
+
+    private static final String JVM_FILES = "jvm.files";
+
+    private static final String JVM_BUFFERS = "jvm.buffers";
+
+    private static final String JVM_CLASSLOADING = "jvm.classloading";
+
+    public static void registerJvmMetric( String nodePrefix, MetricRegistry registry )
+    {
+        registry.register( name( nodePrefix, JVM_MEMORY ), new MemoryUsageGaugeSet() );
+        registry.register( name( nodePrefix, JVM_GARBAGE ), new GarbageCollectorMetricSet() );
+        registry.register( name( nodePrefix, JVM_THREADS ), new ThreadStatesGaugeSet() );
+        registry.register( name( nodePrefix, JVM_FILES ), new FileDescriptorRatioGauge() );
+        registry.register( name( nodePrefix, JVM_CLASSLOADING ), new ClassLoadingGaugeSet() );
+        registry.register( name( nodePrefix, JVM_BUFFERS ),
+                           new BufferPoolMetricSet( ManagementFactory.getPlatformMBeanServer() ) );
+    }
+}
---
+++ subsys/metrics/reporter/src/test/java/org/commonjava/indy/metrics/socket/ZabbixSocketServer.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) 2011-2018 Red Hat, Inc. (https://github.com/Commonjava/indy)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.metrics.socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.commonjava.test.http.util.PortFinder.findOpenPort;
+
+/**
+ * Created by xiabai on 5/9/17.
+ */
+public class ZabbixSocketServer
+                implements Runnable
+{
+    private ConcurrentHashMap<String, Expectation> expections = new ConcurrentHashMap<String, Expectation>();
+
+    private int port;
+
+    public boolean isStartFlag()
+    {
+        return startFlag;
+    }
+
+    public void setStartFlag( boolean startFlag )
+    {
+        this.startFlag = startFlag;
+    }
+
+    private boolean startFlag = true;
+
+    public Expectation getExpection( String method )
+    {
+        return expections.get( method );
+    }
+
+    public void setExpection( String method, Expectation expectation )
+    {
+        expections.put( method, expectation );
+    }
+
+    public int getPort()
+    {
+        return port;
+    }
+
+    public void run()
+    {
+        ServerSocket listener = null;
+        try
+        {
+            int clientNumber = 0;
+            listener = new ServerSocket( findOpenPort( 16 ) );
+            this.port = listener.getLocalPort();
+
+            synchronized ( this )
+            {
+                notifyAll();
+            }
+
+            while ( startFlag )
+            {
+
+                new Thread( new Capitalizer( listener.accept(), clientNumber++, expections ) ).start();
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
+        finally
+        {
+            try
+            {
+                listener.close();
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
