ab754c3de1f83364f435223294a5c2d18cb0f17a
Gang Li <gli@redhat.com>
2019-01-31 17:56:39 +0800

    Add cluster configuration of ispn
    Conflicts:
    	subsys/infinispan/src/main/resources/infinispan-cluster.xml

--- deployments/launcher/src/main/bin/indy.sh
+++ deployments/launcher/src/main/bin/indy.sh
@@ -95,4 +95,4 @@ JAVA_OPTS="$JAVA_OPTS $JAVA_DEBUG_OPTS"
 
 MAIN_CLASS=org.commonjava.indy.boot.jaxrs.JaxRsBooter
 
-exec "$JAVA" ${JAVA_OPTS} -cp "${CP}" ${SECRETS} -Dindy.home="${BASEDIR}" -Dindy.boot.defaults=${BASEDIR}/bin/boot.properties -Dorg.jboss.logging.provider=slf4j -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 ${MAIN_CLASS}  "$@"
+exec "$JAVA" ${JAVA_OPTS} -cp "${CP}" ${SECRETS} -Dindy.home="${BASEDIR}" -Dindy.boot.defaults=${BASEDIR}/bin/boot.properties -Dorg.jboss.logging.provider=slf4j -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 -Djava.net.preferIPv4Stack=true ${MAIN_CLASS} "$@"
