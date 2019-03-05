e8e2457aebaf292f255f2e2fd19685ad09655bc4
Gang Li <gli@redhat.com>
2019-03-05 23:24:04 +0800

    Add Repo change handler

---
+++ subsys/diff/src/main/java/org/commonjava/indy/subsys/diff/RepoChangeHandler.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.subsys.diff;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.difflib.DiffUtils;
+import com.github.difflib.UnifiedDiffUtils;
+import com.github.difflib.algorithm.DiffException;
+import com.github.difflib.patch.Patch;
+import org.commonjava.indy.change.event.ArtifactStorePreUpdateEvent;
+import org.commonjava.indy.data.IndyDataException;
+import org.commonjava.indy.model.change.RepositoryChangeLog;
+import org.commonjava.indy.model.core.ArtifactStore;
+import org.commonjava.indy.model.core.io.IndyObjectMapper;
+import org.commonjava.indy.subsys.diff.cache.RepoChangelogCache;
+import org.commonjava.indy.subsys.infinispan.CacheHandle;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@ApplicationScoped
+public class RepoChangeHandler
+{
+    private static final int DIFF_PATCH_CONTEXT_LINES = 3;
+
+    @Inject
+    private IndyObjectMapper objectMapper;
+
+    @Inject
+    @RepoChangelogCache
+    private CacheHandle<String, RepositoryChangeLog> repoChangelogCache;
+
+    public void generateRepoChangeLog( @Observes ArtifactStorePreUpdateEvent event )
+    {
+        Collection<ArtifactStore> stores = event.getChanges();
+
+        //TODO: think about how to handle these exceptions
+        List<IndyDataException> exceptions = new ArrayList<>();
+        for ( ArtifactStore store : stores )
+        {
+            try
+            {
+                ArtifactStore origin = event.getOriginal( store );
+                String patchString = diffPatchString( store, origin );
+
+                RepositoryChangeLog changeLog = new RepositoryChangeLog();
+                changeLog.setStoreKey( store.getKey() );
+                changeLog.setChangeTime( new Date() );
+                //FIXME: how to define version string and summary?
+                changeLog.setVersion( "" );
+                changeLog.setSummary( "" );
+                //FIXME: seems we don't have a user provider globally
+                changeLog.setUser( "" );
+                changeLog.setDiffContent( patchString );
+
+                String key = changeLog.getStoreKey() + "_" + changeLog.getVersion();
+                repoChangelogCache.put( key, changeLog );
+            }
+            catch ( JsonProcessingException | DiffException e )
+            {
+                String error =
+                        String.format( "Something wrong happened when doing repo change log generation for store %s",
+                                       store.getKey() );
+                exceptions.add( new IndyDataException( error, e ) );
+            }
+        }
+
+    }
+
+    private String diffPatchString( final ArtifactStore changed, final ArtifactStore origin )
+            throws JsonProcessingException, DiffException
+    {
+        String s = objectMapper.writeValueAsString( changed );
+        List<String> storeNewStrings = Arrays.asList( s.split( "\n" ) );
+        s = objectMapper.writeValueAsString( origin );
+        List<String> storeOriginStrings = Arrays.asList( s.split( "\n" ) );
+        Patch<String> patch = DiffUtils.diff( storeNewStrings, storeOriginStrings );
+        String storeFileName = changed.getName() + ".json";
+        List<String> patchDiff =
+                UnifiedDiffUtils.generateUnifiedDiff( "a/" + storeFileName, "b/" + storeFileName, storeOriginStrings,
+                                                      patch, DIFF_PATCH_CONTEXT_LINES );
+        StringBuilder builder = new StringBuilder();
+        patchDiff.forEach( ps -> builder.append( ps ).append( "\n" ) );
+        builder.deleteCharAt( builder.lastIndexOf( "\n" ) );
+        return builder.toString();
+    }
+}
