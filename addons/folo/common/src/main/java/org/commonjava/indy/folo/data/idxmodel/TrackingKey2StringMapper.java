e2227b9101fe2e62b6367fa74563e7ed017e184a
Gang Li <gli@redhat.com>
2019-03-07 15:08:28 +0800

    Fix TrackingKey mapper for WrappedByteArray type
       Add WrappedByteArray type check in TrackingKey2StringMapper which
    refers ISPN DefaultTwoWayKey2StringMapper implementation. And also
    adjust some ISPN config for default settings.

--- addons/folo/common/src/main/java/org/commonjava/indy/folo/data/idxmodel/TrackingKey2StringMapper.java
+++ addons/folo/common/src/main/java/org/commonjava/indy/folo/data/idxmodel/TrackingKey2StringMapper.java
@@ -17,18 +17,26 @@ package org.commonjava.indy.folo.data.idxmodel;
 
 import org.apache.commons.lang3.StringUtils;
 import org.commonjava.indy.folo.model.TrackingKey;
+import org.infinispan.commons.marshall.WrappedByteArray;
 import org.infinispan.persistence.keymappers.TwoWayKey2StringMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TrackingKey2StringMapper implements TwoWayKey2StringMapper
+import java.util.Base64;
+
+public class TrackingKey2StringMapper
+        implements TwoWayKey2StringMapper
 {
     private final Logger LOGGER = LoggerFactory.getLogger( this.getClass() );
+
+    private static final char NON_STRING_PREFIX = '\uFEFF';
+
+    private static final char BYTEARRAYKEY_IDENTIFIER = '8';
 
     @Override
     public boolean isSupportedType( Class<?> keyType )
     {
-        return keyType == TrackingKey.class;
+        return keyType == TrackingKey.class || keyType == WrappedByteArray.class;
     }
 
     @Override
@@ -40,9 +48,19 @@ public class TrackingKey2StringMapper implements TwoWayKey2StringMapper
             return tk.getId();
 
         }
+        else if ( key instanceof WrappedByteArray )
+        {
+            return generateString( BYTEARRAYKEY_IDENTIFIER,
+                                   Base64.getEncoder().encodeToString( ( (WrappedByteArray) key ).getBytes() ) );
+        }
         LOGGER.error( "Folo tracking JDBC store error: Not supported key type {}",
                       key == null ? null : key.getClass() );
         return null;
+    }
+
+    private String generateString( char identifier, String s )
+    {
+        return String.valueOf( NON_STRING_PREFIX ) + String.valueOf( identifier ) + s;
     }
 
     @Override
