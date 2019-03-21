3d5646d679f65d4d59fb5c557b839aa1066ba899
Gang Li <ligangty@gmail.com>
2019-03-22 03:42:08 +0800

    [Port to master] Support jdbc store for folo-sealed ISPN cache (#1180)
    * Support jdbc store for folo-sealed ISPN cache
    
    * Fix TrackingKey mapper for WrappedByteArray type
    
       Add WrappedByteArray type check in TrackingKey2StringMapper which
    refers ISPN DefaultTwoWayKey2StringMapper implementation. And also
    adjust some ISPN config for default settings.
    
    * Fix way of WrappedByteArray for TrackingKey by using ObjInputStream
    
    * Use externalize but not direct obj stream of TrackingKey2StringMapper
    
    * Extract abstract class for key2StringMapper with WrappedArrayByte handling

---
+++ addons/folo/common/src/main/java/org/commonjava/indy/folo/data/idxmodel/TrackingKey2StringMapper.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2013~2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.folo.data.idxmodel;
+
+import org.apache.commons.lang3.StringUtils;
+import org.commonjava.indy.folo.model.TrackingKey;
+import org.commonjava.indy.subsys.infinispan.AbstractIndyKey2StringMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TrackingKey2StringMapper
+        extends AbstractIndyKey2StringMapper<TrackingKey>
+{
+    private final Logger LOGGER = LoggerFactory.getLogger( this.getClass() );
+
+    @Override
+    public Object getKeyMapping( String stringKey )
+    {
+        if ( StringUtils.isNotEmpty( stringKey ) )
+        {
+            return new TrackingKey( stringKey );
+        }
+        else
+        {
+            LOGGER.error( "Folo tracking JDBC store error: an empty store key from store" );
+            return null;
+        }
+    }
+
+    @Override
+    protected String getStringMappingFromInst( Object key )
+    {
+        if ( !( key instanceof TrackingKey ) )
+        {
+            return null;
+        }
+        return ( (TrackingKey) key ).getId();
+    }
+
+    @Override
+    protected Class<TrackingKey> provideKeyClass()
+    {
+        return TrackingKey.class;
+    }
+}
---
+++ addons/folo/common/src/test/java/org/commonjava/indy/folo/data/TrackingKey2StringMapperTest.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2013~2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.folo.data;
+
+import org.commonjava.indy.folo.data.idxmodel.TrackingKey2StringMapper;
+import org.commonjava.indy.folo.model.TrackedContent;
+import org.commonjava.indy.folo.model.TrackingKey;
+import org.infinispan.commons.marshall.WrappedByteArray;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+public class TrackingKey2StringMapperTest
+{
+    private static final String TEST_ID = "testid";
+
+    @Test
+    public void testMapper()
+            throws IOException
+    {
+        TrackingKey2StringMapper mapper = new TrackingKey2StringMapper();
+        TrackingKey key = new TrackingKey( TEST_ID );
+
+        Assert.assertTrue( mapper.isSupportedType( TrackingKey.class ) );
+        Assert.assertFalse( mapper.isSupportedType( Object.class ) );
+        Assert.assertFalse( mapper.isSupportedType( TrackedContent.class ) );
+        Assert.assertTrue( mapper.isSupportedType( WrappedByteArray.class ) );
+
+        Object genKey = mapper.getKeyMapping( TEST_ID );
+        Assert.assertEquals( key, genKey );
+
+        String genId = mapper.getStringMapping( key );
+        Assert.assertEquals( TEST_ID, genId );
+
+        WrappedByteArray array = bytesOfTrackingKey( key );
+        genId = mapper.getStringMapping( array );
+        Assert.assertEquals( TEST_ID, genId );
+
+    }
+
+    private WrappedByteArray bytesOfTrackingKey( TrackingKey key )
+            throws IOException
+    {
+        byte[] bytes = null;
+        try (ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+             ObjectOutputStream stream = new ObjectOutputStream( byteOut ))
+        {
+            key.writeExternal( stream );
+            bytes = byteOut.toByteArray();
+            return new WrappedByteArray( bytes, Arrays.hashCode( bytes ) );
+        }
+    }
+}
