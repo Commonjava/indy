27d2901ecbc60f950e386b753ea7b63207c31eb0
John Casey <jdcasey@commonjava.org>
2019-04-05 15:03:41 -0500

    remove unused constant

--- subsys/jaxrs/src/main/java/org/commonjava/indy/bind/jaxrs/RequestContextConstants.java
+++ subsys/jaxrs/src/main/java/org/commonjava/indy/bind/jaxrs/RequestContextConstants.java
@@ -60,7 +60,4 @@ public class RequestContextConstants
 
     @Thread @MDC
     public static final String PREFERRED_ID = "preferred-id";
-
-    @Thread @MDC
-    public static final String ENVIRONMENT = "environment";
 }
