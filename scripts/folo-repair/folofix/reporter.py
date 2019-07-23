c416b4a3fd703e9d66bfe0fa6c92af5a0756ed01
John Casey <jdcasey@commonjava.org>
2019-07-23 13:44:05 -0500

    update license headers to 2019

--- scripts/folo-repair/folofix/reporter.py
+++ scripts/folo-repair/folofix/reporter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2011-2018 Red Hat, Inc. (https://github.com/Commonjava/indy)
+# Copyright (C) 2011-2019 Red Hat, Inc. (https://github.com/Commonjava/indy)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
