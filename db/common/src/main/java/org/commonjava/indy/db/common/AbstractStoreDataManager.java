4ea19c817f2f515292c150f03627563d8e5b5d30
Gang Li <gli@redhat.com>
2019-12-20 15:17:20 +0800

    Extract getGroupsAffectedBy from Query to StoreDataManager

--- db/common/src/main/java/org/commonjava/indy/db/common/AbstractStoreDataManager.java
+++ db/common/src/main/java/org/commonjava/indy/db/common/AbstractStoreDataManager.java
@@ -28,6 +28,7 @@ import org.commonjava.indy.data.StoreEventDispatcher;
 import org.commonjava.indy.data.StoreValidator;
 import org.commonjava.indy.measure.annotation.Measure;
 import org.commonjava.indy.model.core.ArtifactStore;
+import org.commonjava.indy.model.core.Group;
 import org.commonjava.indy.model.core.HostedRepository;
 import org.commonjava.indy.model.core.StoreKey;
 import org.commonjava.indy.model.core.StoreType;
@@ -37,7 +38,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
@@ -422,4 +427,74 @@ public abstract class AbstractStoreDataManager
         return streamArtifactStoreKeys().filter( key -> key.getPackageType().equals( pkg ) && key.getType() == type )
                                         .collect( Collectors.toSet() );
     }
+
+    public Set<Group> affectedBy( final Collection<StoreKey> keys )
+            throws IndyDataException
+    {
+        return affectedByFromStores( keys );
+    }
+
+    protected Set<Group> affectedByFromStores( final Collection<StoreKey> keys )
+            throws IndyDataException
+    {
+        Logger logger = LoggerFactory.getLogger( getClass() );
+        logger.debug( "Getting groups affected by: {}", keys );
+
+        List<StoreKey> toProcess = new ArrayList<>( new HashSet<>( keys ) );
+
+        Set<Group> groups = new HashSet<>();
+        if ( toProcess.isEmpty() )
+        {
+            return groups;
+        }
+
+        Set<StoreKey> processed = new HashSet<>();
+        final String packageType = toProcess.get( 0 ).getPackageType();
+
+        Set<ArtifactStore> all = this.getAllArtifactStores().stream().filter( st -> {
+            if ( packageType != null && !st.getPackageType().equals( packageType ) )
+            {
+                return false;
+            }
+
+            if ( st.getType() != StoreType.group )
+            {
+                return false;
+            }
+
+            return true;
+        } ).collect( Collectors.toSet() );
+
+        while ( !toProcess.isEmpty() )
+        {
+            // as long as we have another key to process, pop it off the list (remove it) and process it.
+            StoreKey next = toProcess.remove( 0 );
+            if ( processed.contains( next ) )
+            {
+                // if we've already handled this group (via another branch in the group membership tree, etc. then don't bother.
+                continue;
+            }
+
+            // use this to avoid reprocessing groups we've already encountered.
+            processed.add( next );
+
+            for ( ArtifactStore store : all )
+            {
+                if ( ( store instanceof Group ) && !processed.contains( store.getKey() )  )
+                {
+                    Group g = (Group) store;
+                    if ( g.getConstituents() != null && g.getConstituents().contains( next ) )
+                    {
+                        groups.add( g );
+
+                        // add this group as another one to process for groups that contain it...and recurse upwards
+                        toProcess.add( g.getKey() );
+                    }
+                }
+            }
+        }
+
+        return groups;
+    }
+
 }
