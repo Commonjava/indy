f1c39d442a094a82d6312a7ba967b1424534f3e8
Gang Li <gli@redhat.com>
2020-03-02 23:14:23 +0800

    Repository proxy addon, provides following functions
      * Rewrites api url based on the rule of the repository proxy.(from one
        repo to another)
      * Controls which apis will be handled in rewriting, based on api url
        patterns in configuration
      * Controls which http methods can be included in rewriting, based on
        configuration
      * Can be enabled/disabled

---
+++ addons/repo-proxy/common/src/main/java/org/commonjava/indy/repo/proxy/conf/RepoProxyConfig.java
@@ -0,0 +1,170 @@
+/**
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.repo.proxy.conf;
+
+import org.commonjava.indy.conf.IndyConfigInfo;
+import org.commonjava.indy.repo.proxy.RepoProxyAddon;
+import org.commonjava.propulsor.config.ConfigurationException;
+import org.commonjava.propulsor.config.annotation.SectionName;
+import org.commonjava.propulsor.config.section.MapSectionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.io.File;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.commonjava.indy.repo.proxy.RepoProxyAddon.ADDON_NAME;
+
+@SectionName( RepoProxyConfig.SECTION )
+@ApplicationScoped
+public class RepoProxyConfig
+        extends MapSectionListener
+        implements IndyConfigInfo
+{
+    private final Logger logger = LoggerFactory.getLogger( getClass() );
+
+    private static final String DEFAULT_CONFIG_FILE_NAME = "default-repo-proxy.conf";
+
+    public static final String SECTION = "repo-proxy";
+
+    private static final String ENABLED_PARAM = "enabled";
+
+    private static final String API_PATTERNS_PARAM = "api.url.patterns";
+
+    private static final String API_METHODS_PARAM = "api.methods";
+
+    private static final Boolean DEFAULT_ENABLED = Boolean.FALSE;
+
+    private static final String DEFAULT_API_PATTERNS = "/api/content/*, /api/folo/track/*";
+
+    private static final String DEFAULT_API_METHODS = "GET,HEAD";
+
+    private static final String PROXY_KEY_PREFIX = "proxy.";
+
+    private final Map<String, String> proxyRules = new LinkedHashMap<>();
+
+    private final Set<String> apiPatterns = new HashSet<>();
+
+    private final Set<String> apiMethods = new HashSet<>();
+
+    private Boolean enabled;
+
+    public RepoProxyConfig()
+    {
+    }
+
+    public boolean getEnabled()
+    {
+        return this.enabled == null ? DEFAULT_ENABLED : this.enabled;
+    }
+
+    public boolean isEnabled()
+    {
+        return getEnabled();
+    }
+
+    public void setEnabled( Boolean enabled )
+    {
+        this.enabled = enabled;
+    }
+
+    public Map<String, String> getProxyRules()
+    {
+        return proxyRules;
+    }
+
+    public Set<String> getApiPatterns()
+    {
+        if ( apiPatterns.isEmpty() )
+        {
+            for ( String pattern : DEFAULT_API_PATTERNS.split( "," ) )
+            {
+                apiPatterns.add( pattern.trim() );
+            }
+        }
+        return apiPatterns;
+    }
+
+    public Set<String> getApiMethods()
+    {
+        if ( apiMethods.isEmpty() )
+        {
+            for ( String method : DEFAULT_API_METHODS.split( "," ) )
+            {
+                apiMethods.add( method.trim().toUpperCase() );
+            }
+        }
+        return apiMethods;
+    }
+
+    @Override
+    public void parameter( final String name, final String value )
+            throws ConfigurationException
+    {
+        logger.trace( "{}: config tracing: name({})-value({}) ", ADDON_NAME, name, value );
+        switch ( name )
+        {
+            case ENABLED_PARAM:
+                this.enabled = Boolean.valueOf( value.trim() );
+                break;
+            case API_PATTERNS_PARAM:
+                apiPatterns.clear();
+                for ( String pattern : value.split( "," ) )
+                {
+                    apiPatterns.add( pattern.trim() );
+                }
+                break;
+            case API_METHODS_PARAM:
+                apiMethods.clear();
+                for ( String method : value.split( "," ) )
+                {
+                    apiMethods.add( method.trim().toUpperCase() );
+                }
+                break;
+            default:
+            {
+                if ( name.startsWith( PROXY_KEY_PREFIX ) && name.length() > PROXY_KEY_PREFIX.length() )
+                {
+                    String source = name.substring( PROXY_KEY_PREFIX.length() );
+                    String sourceRepoPath = source.replaceAll( "\\.", "/" );
+                    logger.trace( "{}: Repo {} proxy target is repo {}", ADDON_NAME, source, value );
+                    proxyRules.put( sourceRepoPath, value.replaceAll( ":", "/" ) );
+                }
+                else
+                {
+                    throw new ConfigurationException( "Invalid parameter: '%s'.", value, name, SECTION );
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getDefaultConfigFileName()
+    {
+        return new File( IndyConfigInfo.CONF_INCLUDES_DIR, DEFAULT_CONFIG_FILE_NAME ).getPath();
+    }
+
+    @Override
+    public InputStream getDefaultConfig()
+    {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream( DEFAULT_CONFIG_FILE_NAME );
+    }
+}
---
+++ addons/repo-proxy/ftests/src/main/java/org/commonjava/indy/repo/proxy/ftest/RepoProxyDisableTest.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.indy.repo.proxy.ftest;
+
+import org.apache.commons.io.IOUtils;
+import org.commonjava.indy.ftest.core.AbstractContentManagementTest;
+import org.commonjava.indy.model.core.HostedRepository;
+import org.commonjava.indy.model.core.RemoteRepository;
+import org.commonjava.indy.pkg.maven.model.MavenPackageTypeDescriptor;
+import org.commonjava.indy.test.fixture.core.CoreServerFixture;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Check if the repo proxy addon will stop working when disabled.
+ * <br/>
+ * GIVEN:
+ * <ul>
+ *     <li>Hosted and Remote Repo</li>
+ *     <li>Hosted does not contain pathA but remote does</li>
+ *     <li>Hosted contains pathB but remote does not</li>
+ *     <li>Configured the repo-proxy disabled</li>
+ *     <li>Configured proxy rule with mapping hosted proxy to remote</li>
+ * </ul>
+ * <br/>
+ * WHEN:
+ * <ul>
+ *     <li>Request pathA and pathB through hosted</li>
+ *     <li>Request pathA and pathB through remote</li>
+ * </ul>
+ * <br/>
+ * THEN:
+ * <ul>
+ *     <li>The content of pathA can not be returned correctly from hosted, but pathB can</li>
+ *     <li>The content of pathA can not be returned correctly from remote, but pathB can not</li>
+ * </ul>
+ */
+public class RepoProxyDisableTest
+        extends AbstractContentManagementTest
+{
+    private static final String REPO_NAME = "pnc-builds";
+
+    private HostedRepository hosted;
+
+    private RemoteRepository remote;
+
+    private static final String PATH1 = "foo/bar/1.0/foo-bar-1.0.txt";
+
+    private static final String PATH2 = "foo/bar/2.0/foo-bar-2.0.txt";
+
+    private static final String CONTENT1 = "This is content 1";
+
+    private static final String CONTENT2 = "This is content 2";
+
+    @Before
+    public void setupRepos()
+            throws Exception
+    {
+
+        server.expect( server.formatUrl( REPO_NAME, PATH1 ), 200, new ByteArrayInputStream( CONTENT1.getBytes() ) );
+
+        remote = client.stores()
+                       .create( new RemoteRepository( MavenPackageTypeDescriptor.MAVEN_PKG_KEY, REPO_NAME,
+                                                      server.formatUrl( REPO_NAME ) ), "remote pnc-builds",
+                                RemoteRepository.class );
+
+        hosted = client.stores()
+                       .create( new HostedRepository( MavenPackageTypeDescriptor.MAVEN_PKG_KEY, REPO_NAME ),
+                                "hosted pnc-builds", HostedRepository.class );
+        client.content().store( hosted.getKey(), PATH2, new ByteArrayInputStream( CONTENT2.getBytes() ) );
+    }
+
+    @Test
+    public void run()
+            throws Exception
+    {
+        InputStream result = client.content().get( hosted.getKey(), PATH1 );
+        assertThat( result, nullValue() );
+
+        try (InputStream result2 = client.content().get( hosted.getKey(), PATH2 ))
+        {
+            assertThat( result2, notNullValue() );
+            final String content = IOUtils.toString( result2 );
+            assertThat( content, equalTo( CONTENT2 ) );
+        }
+
+        try (InputStream result3 = client.content().get( remote.getKey(), PATH1 ))
+        {
+            assertThat( result3, notNullValue() );
+            final String content = IOUtils.toString( result3 );
+            assertThat( content, equalTo( CONTENT1 ) );
+        }
+
+        result = client.content().get( remote.getKey(), PATH2 );
+        assertThat( result, nullValue() );
+
+    }
+
+    @Override
+    protected void initTestConfig( CoreServerFixture fixture )
+            throws IOException
+    {
+        writeConfigFile( "conf.d/repo-proxy.conf",
+                         "[repo-proxy]\nproxy.maven.hosted.pnc-builds=maven:remote:pnc-builds" );
+    }
+
+}
