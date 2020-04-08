1c747d1e3d033c2a4cf14028c71a05fd1ee8b2cd
Gang Li <gli@redhat.com>
2020-04-08 10:30:50 +0800

    Fix a merge conflict

--- addons/repo-proxy/common/src/main/java/org/commonjava/indy/repo/proxy/RepoProxyUtils.java
+++ addons/repo-proxy/common/src/main/java/org/commonjava/indy/repo/proxy/RepoProxyUtils.java
@@ -1,14 +1,5 @@
 /**
  * Copyright (C) 2013 Red Hat, Inc.
- <<<<<<< HEAD
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- =======
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +7,6 @@
  *
  *         http://www.apache.org/licenses/LICENSE-2.0
  *
- >>>>>>> f3674f198... Controlling NPM metadata content by repo-proxy
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
