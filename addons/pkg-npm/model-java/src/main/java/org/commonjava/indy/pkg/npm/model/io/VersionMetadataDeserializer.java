3b6d28a8ddc4b6a52b6b3a8fbecc5c8aa8956404
Wenjie Guo <wguo@redhat.com>
2020-04-27 17:05:09 +0800

    Add custom deserializer for VersionMetadata
    Adding this aims to handle the parsing stuff more flexible I hope,
    seems the rules of package metadata template is loose/open, I see
    most of the repository of version metadata is a object containing
    "type" and "url",  but we ran into the pakcage "tmp" which the repository
    is a array, and that appears only in one of their versions(e.g.: 0.0.4).
    Details see: http://registry.npmjs.org/tmp
    
    I don't think we need to update our model to adapt this type of data,
    but we have to consider it because it will break our code and cause
    no versions for the package.

---
+++ addons/pkg-npm/model-java/src/main/java/org/commonjava/indy/pkg/npm/model/io/VersionMetadataDeserializer.java
@@ -0,0 +1,99 @@
+package org.commonjava.indy.pkg.npm.model.io;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.commonjava.indy.pkg.npm.model.Bugs;
+import org.commonjava.indy.pkg.npm.model.Directories;
+import org.commonjava.indy.pkg.npm.model.Dist;
+import org.commonjava.indy.pkg.npm.model.Engines;
+import org.commonjava.indy.pkg.npm.model.License;
+import org.commonjava.indy.pkg.npm.model.Repository;
+import org.commonjava.indy.pkg.npm.model.UserInfo;
+import org.commonjava.indy.pkg.npm.model.VersionMetadata;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class VersionMetadataDeserializer extends StdDeserializer<VersionMetadata>
+{
+
+    public VersionMetadataDeserializer()
+    {
+        this(null);
+    }
+
+    protected VersionMetadataDeserializer( Class<?> vc )
+    {
+        super( vc );
+    }
+
+    @Override
+    public VersionMetadata deserialize( JsonParser jsonParser, DeserializationContext deserializationContext )
+                    throws IOException
+    {
+        final ObjectMapper mapper = (ObjectMapper) jsonParser.getCodec();
+        final JsonNode vNode = mapper.readTree( jsonParser );
+
+        VersionMetadata vm = new VersionMetadata( parseValue( vNode, "name" ), parseValue( vNode, "version" ) );
+        vm.setDescription( parseValue( vNode, "description" ) );
+
+        JsonNode repoNode = vNode.get( "repository" );
+        if ( repoNode instanceof ArrayNode )
+        {
+            repoNode = repoNode.get( 0 );
+        }
+        vm.setRepository( new Repository( parseValue( repoNode, "type"), parseValue( repoNode, "url") ) );
+
+        vm.setAuthor( parseObject( mapper, vNode.get( "author" ), UserInfo.class ));
+        vm.setBugs( parseObject( mapper, vNode.get( "bugs" ), Bugs.class ) );
+        vm.setDist( parseObject( mapper, vNode.get( "dist" ), Dist.class ) );
+        vm.setDirectories( parseObject( mapper, vNode.get( "directories" ), Directories.class ) );
+        vm.setKeywords( parseList( mapper, vNode.get( "keywords" ), String.class ) );
+        vm.setLicense( parseObject( mapper, vNode.get( "license" ), License.class ) );
+        vm.setMain( parseValue( vNode, "main" ) );
+
+        vm.setContributors( parseList( mapper, vNode.get( "contributors" ), UserInfo.class ));
+        vm.setEngines( parseList( mapper, vNode.get( "engines" ), Engines.class ));
+
+        vm.setDependencies( parseObject( mapper, vNode.get( "dependencies" ), Map.class ) );
+        vm.setDevDependencies( parseObject( mapper, vNode.get( "devDependencies" ), Map.class ) );
+
+        vm.setMaintainers( parseList( mapper, vNode.get( "maintainers" ), UserInfo.class));
+        vm.setLicenses( parseList( mapper, vNode.get( "licenses" ), License.class ) );
+
+        vm.setScripts( parseObject( mapper, vNode.get( "scripts" ), Map.class ) );
+
+        return vm;
+    }
+
+    private <T> T parseObject( ObjectMapper mapper, JsonNode node, Class<T> tClass ) throws JsonProcessingException
+    {
+        return mapper.treeToValue( node, tClass );
+    }
+
+    public <T> List<T> parseList( ObjectMapper mapper, JsonNode node, Class<T> tClass ) throws IOException
+    {
+        if ( node == null )
+        {
+            return null;
+        }
+
+        return mapper.readValue( mapper.treeAsTokens( node ),
+                                 mapper.getTypeFactory().constructCollectionType( List.class, tClass ) );
+    }
+
+    private String parseValue( JsonNode repoNode, String key )
+    {
+        if ( repoNode!=null && repoNode.get( key ) != null )
+        {
+            return repoNode.get( key ).asText();
+        }
+        return null;
+    }
+}
