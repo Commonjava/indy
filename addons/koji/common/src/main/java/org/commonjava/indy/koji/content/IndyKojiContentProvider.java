e4f14b85bcc4a6f2d3d36e677d1049e1d0c322eb
Gang Li <gli@redhat.com>
2020-05-28 19:35:25 +0800

    Add tracing logs for debugging in future in koji addon

--- addons/koji/common/src/main/java/org/commonjava/indy/koji/content/IndyKojiContentProvider.java
+++ addons/koji/common/src/main/java/org/commonjava/indy/koji/content/IndyKojiContentProvider.java
@@ -132,6 +132,7 @@ public class IndyKojiContentProvider
             List ret = cache.get( buildId );
             if ( ret != null )
             {
+                logger.trace( "Build {} got its tags from tags query cache, no need to query from koji again", buildId );
                 map.put( buildId, ret );
             }
             else
@@ -141,7 +142,15 @@ public class IndyKojiContentProvider
         }
         if ( !missed.isEmpty() )
         {
+            logger.trace( "Builds which did not find tags in tags query cache: {}", missed );
             Map<Integer, List<KojiTagInfo>> retrieved = kojiClient.listTags( missed, session );
+            if ( logger.isTraceEnabled() )
+            {
+                for ( Map.Entry<Integer, List<KojiTagInfo>> entry : retrieved.entrySet() )
+                {
+                    logger.trace( "Build {} has {} tags from remote koji", entry.getKey(), entry.getValue().size() );
+                }
+            }
             map.putAll( retrieved );
             retrieved.forEach( ( k, v ) -> cache.put( k, v ) );
         }
