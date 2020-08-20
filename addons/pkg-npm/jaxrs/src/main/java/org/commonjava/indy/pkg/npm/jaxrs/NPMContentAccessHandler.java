a747c066ac1beb891a8f400ce59c278d2a9bdec1
Petr Kocandrle <pkocandr@redhat.com>
2020-08-20 14:05:30 +0200

    [NCL-5937] Fix storage path for uploaded scoped NPM packages

--- addons/pkg-npm/jaxrs/src/main/java/org/commonjava/indy/pkg/npm/jaxrs/NPMContentAccessHandler.java
+++ addons/pkg-npm/jaxrs/src/main/java/org/commonjava/indy/pkg/npm/jaxrs/NPMContentAccessHandler.java
@@ -427,6 +427,12 @@ public class NPMContentAccessHandler
                 {
                     return null;
                 }
+                // remove scope if present
+                if ( tarball.startsWith( '@' ) && tarball.contains( '/' ) )
+                {
+                    tarball = tarball.split( '/', 2 )[1];
+                }
+
                 tarballPath = Paths.get( idnode.asText(), "-", tarball ).toString();
                 tarballContent = anode.findPath( "data" ).asText();
             }
