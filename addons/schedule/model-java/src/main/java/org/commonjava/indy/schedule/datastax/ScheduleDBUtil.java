63edad9f0f5efe06d8cc82d19350f635c073ace4
Wenjie Guo <wguo@redhat.com>
2020-08-20 14:14:10 +0800

    Add jobtype as index for schedule

--- addons/schedule/model-java/src/main/java/org/commonjava/indy/schedule/datastax/ScheduleDBUtil.java
+++ addons/schedule/model-java/src/main/java/org/commonjava/indy/schedule/datastax/ScheduleDBUtil.java
@@ -3,9 +3,11 @@ package org.commonjava.indy.schedule.datastax;
 public class ScheduleDBUtil
 {
 
+    public static final String TABLE_SCHEDULE = "schedule";
+
     public static String getSchemaCreateTableSchedule( String keyspace )
     {
-        return "CREATE TABLE IF NOT EXISTS " + keyspace + ".schedule ("
+        return "CREATE TABLE IF NOT EXISTS " + keyspace + "." + TABLE_SCHEDULE + " ("
                         + "jobtype varchar,"
                         + "jobname varchar,"
                         + "storekey varchar,"
@@ -14,6 +16,11 @@ public class ScheduleDBUtil
                         + "expiration varchar,"
                         + "PRIMARY KEY (storekey, jobname)"
                         + ");";
+    }
+
+    public static String getSchemaCreateTypeIndex4Schedule( String keyspace )
+    {
+        return "CREATE INDEX IF NOT EXISTS type_idx on " + keyspace + "." + TABLE_SCHEDULE + " (jobtype)";
     }
 
 }
