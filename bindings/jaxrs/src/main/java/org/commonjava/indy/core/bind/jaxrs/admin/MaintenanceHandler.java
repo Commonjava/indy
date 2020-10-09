616efd9d9d1bc925078a0fe72fd35e5aa8f66aa3
Wenjie Guo <wguo@redhat.com>
2020-10-09 15:21:13 +0800

    Introduce the interface to import store zip

--- bindings/jaxrs/src/main/java/org/commonjava/indy/core/bind/jaxrs/admin/MaintenanceHandler.java
+++ bindings/jaxrs/src/main/java/org/commonjava/indy/core/bind/jaxrs/admin/MaintenanceHandler.java
@@ -19,19 +19,24 @@ import static org.commonjava.indy.pkg.maven.model.MavenPackageTypeDescriptor.MAV
 import static org.commonjava.indy.util.ApplicationContent.application_json;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.commonjava.indy.IndyWorkflowException;
 import org.commonjava.indy.bind.jaxrs.IndyResources;
 import org.commonjava.indy.bind.jaxrs.util.REST;
@@ -50,6 +55,7 @@ import org.commonjava.maven.galley.event.EventMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
@@ -80,6 +86,7 @@ public class MaintenanceHandler
 
     @Inject
     private ResponseHelper responseHelper;
+
 
     @ApiOperation( "[Deprecated] Rescan all content in the specified repository to re-initialize metadata, capture missing index keys, etc." )
     @ApiResponse( code = 200, message = "Rescan was started successfully. (NOTE: There currently is no way to determine when rescanning is complete.)" )
@@ -310,6 +317,24 @@ public class MaintenanceHandler
     public Response doDelete( final BatchDeleteRequest request )
     {
         return contentAccessHandler.doDelete( request, new EventMetadata(  ) );
+    }
+
+    @ApiOperation( "Import artifact stores from a ZIP file." )
+    @ApiResponses( { @ApiResponse( code = 201, message = "Import ZIP content" ) } )
+    @Path( "/store/import" )
+    @PUT
+    public Response importStore( final @Context UriInfo uriInfo, final @Context HttpServletRequest request )
+    {
+        try
+        {
+            maintenanceController.importStoreZip( request.getInputStream() );
+        }
+        catch ( IOException e )
+        {
+            responseHelper.throwError( new IndyWorkflowException( "IO error", e ) );
+        }
+
+        return Response.created( uriInfo.getRequestUri() ).build();
     }
 
 }
