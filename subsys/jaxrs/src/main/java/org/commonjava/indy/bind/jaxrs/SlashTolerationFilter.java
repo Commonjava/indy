7a98a10dd10a54762c495af1e367035b7127001d
shokakucarrier <commonjava-bot@commonjava.org>
2020-10-14 09:10:25 +0000

    Update license header

--- subsys/jaxrs/src/main/java/org/commonjava/indy/bind/jaxrs/SlashTolerationFilter.java
+++ subsys/jaxrs/src/main/java/org/commonjava/indy/bind/jaxrs/SlashTolerationFilter.java
@@ -12,8 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.commonjava.indy.bind.jaxrs;
-
+ */
 import javax.enterprise.context.ApplicationScoped;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
