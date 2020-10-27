fc3a64ffb314e54027eb28b4aa758f741c44b1ec
John Casey <jdcasey@commonjava.org>
2020-10-27 17:32:40 -0500

    fixing REST API generation

--- addons/repo-proxy/common/src/main/java/org/commonjava/indy/repo/proxy/content/RepoProxyCacheProducer.java
+++ addons/repo-proxy/common/src/main/java/org/commonjava/indy/repo/proxy/content/RepoProxyCacheProducer.java
@@ -1,6 +1,5 @@
 package org.commonjava.indy.repo.proxy.content;
 
-import org.commonjava.indy.core.expire.ScheduleKey;
 import org.commonjava.indy.subsys.infinispan.CacheHandle;
 import org.commonjava.indy.subsys.infinispan.CacheProducer;
 import org.slf4j.Logger;
@@ -9,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
-import java.util.Map;
 
 public class RepoProxyCacheProducer
 {
@@ -23,7 +21,7 @@ public class RepoProxyCacheProducer
     @RepoProxyContentCache
     @Produces
     @ApplicationScoped
-    public CacheHandle<ScheduleKey, Map> scheduleExpireCache()
+    public CacheHandle<String, String> scheduleExpireCache()
     {
         return cacheProducer.getCache( REPO_PROXY_CONTENT_CACHE );
     }
