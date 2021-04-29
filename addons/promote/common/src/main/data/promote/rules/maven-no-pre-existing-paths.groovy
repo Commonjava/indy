70029ea98fea85d99d4c5a7c136232af9d10b2fe
Wenjie Guo <wguo@redhat.com>
2021-04-29 15:42:10 +0800

    Support idempotent promotion

--- addons/promote/common/src/main/data/promote/rules/maven-no-pre-existing-paths.groovy
+++ addons/promote/common/src/main/data/promote/rules/maven-no-pre-existing-paths.groovy
@@ -1,9 +1,11 @@
 package org.commonjava.indy.promote.rules
 
 import org.apache.commons.lang.StringUtils
+import org.commonjava.indy.pkg.maven.model.MavenPackageTypeDescriptor
 import org.commonjava.indy.promote.validate.PromotionValidationException
 import org.commonjava.indy.promote.validate.model.ValidationRequest
 import org.commonjava.indy.promote.validate.model.ValidationRule
+import org.commonjava.maven.galley.io.checksum.ContentDigest
 
 class MavenNoPreExistingPaths implements ValidationRule {
 
@@ -12,12 +14,13 @@ class MavenNoPreExistingPaths implements ValidationRule {
 
         def errors = Collections.synchronizedList(new ArrayList());
         def tools = request.getTools()
-
         tools.paralleledEach(request.getSourcePaths(), { it ->
             def aref = tools.getArtifact(it);
             if (aref != null) {
                 tools.forEach(verifyStoreKeys, { verifyStoreKey ->
-                    if (tools.exists(verifyStoreKey, it)) {
+                    if (tools.exists(verifyStoreKey, it)
+                            && !(tools.digest(verifyStoreKey, it, MavenPackageTypeDescriptor.MAVEN_PKG_KEY).get(ContentDigest.SHA_256)
+                            .equals(tools.digest(request.getPromoteRequest().getSource(), it, MavenPackageTypeDescriptor.MAVEN_PKG_KEY).get(ContentDigest.SHA_256)))) {
                         errors.add(String.format("%s is already available in: %s", it, verifyStoreKey))
                     }
                 })
