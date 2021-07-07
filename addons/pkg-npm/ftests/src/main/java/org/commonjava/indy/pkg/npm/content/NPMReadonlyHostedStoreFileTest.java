11ab9ef94337484f915668ccfaeb171ac1036808
Rui Han <ruhan@redhat.com>
2021-07-07 10:04:29 +0800

    Allow force delte for npm metadata (#1950)
    * Allow force delte for npm metadata
    
    * Fix npm meta deletion 404 issue and use one single delete REST method

--- addons/pkg-npm/ftests/src/main/java/org/commonjava/indy/pkg/npm/content/NPMReadonlyHostedStoreFileTest.java
+++ addons/pkg-npm/ftests/src/main/java/org/commonjava/indy/pkg/npm/content/NPMReadonlyHostedStoreFileTest.java
@@ -15,6 +15,7 @@
  */
 package org.commonjava.indy.pkg.npm.content;
 
+import org.apache.commons.io.IOUtils;
 import org.commonjava.indy.client.core.IndyClientException;
 import org.commonjava.indy.ftest.core.AbstractContentManagementTest;
 import org.commonjava.indy.model.core.HostedRepository;
@@ -30,22 +31,21 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 /**
- * This case tests if files can be deleted in a readonly hosted repo
+ * This case tests if files can be stored in a readonly hosted repo
  * when: <br />
  * <ul>
- *      <li>creates a non-readonly hosted repo and stores file in it</li>
+ *      <li>creates a readonly hosted repo</li>
+ *      <li>stores file in hosted repo once</li>
  *      <li>updates the hosted repo to non-readonly</li>
- *      <li>deletes the file in hosted repo once</li>
- *      <li>updates the hosted repo to non-readonly</li>
- *      <li>deletes file again</li>
+ *      <li>stores file again</li>
  * </ul>
  * then: <br />
  * <ul>
- *     <li>the file can not be deleted with 405 error first time</li>
- *     <li>the file can be deleted successfully with no error second time</li>
+ *     <li>the file can not be stored with 405 error first time</li>
+ *     <li>the file can be stored successfully with no error second time</li>
  * </ul>
  */
-public class NPMReaonlyHostedDeleteFileTest
+public class NPMReadonlyHostedStoreFileTest
                 extends AbstractContentManagementTest
 {
 
@@ -55,40 +55,41 @@ public class NPMReaonlyHostedDeleteFileTest
         final String content = "This is a test: " + System.nanoTime();
         InputStream stream = new ByteArrayInputStream( content.getBytes() );
 
-        final String path = "jquery/2.1.0";
+        final String path = "jquery/-/jquery-2.1.0.tgz";
 
-        final String repoName = "test-npm-hosted";
+        final String repoName = "test-hosted";
         HostedRepository repo = new HostedRepository( NPM_PKG_KEY, repoName );
+        repo.setReadonly( true );
         repo = client.stores().create( repo, "adding npm hosted repo", HostedRepository.class );
 
         StoreKey storeKey = repo.getKey();
         assertThat( client.content().exists( storeKey, path ), equalTo( false ) );
 
-        client.content().store( storeKey, path, stream );
-
-        assertThat( client.content().exists( storeKey, path ), equalTo( true ) );
-
-        repo.setReadonly( true );
-        client.stores().update( repo, "change read-only true" );
-
         try
         {
-            client.content().delete( storeKey, path );
+            client.content().store( storeKey, path, stream );
         }
         catch ( IndyClientException e )
         {
             assertThat( e.getStatusCode(), equalTo( ApplicationStatus.METHOD_NOT_ALLOWED.code() ) );
         }
 
-        assertThat( client.content().exists( storeKey, path ), equalTo( true ) );
+        assertThat( client.content().exists( storeKey, path ), equalTo( false ) );
 
         repo.setReadonly( false );
         client.stores().update( repo, "change read-only false" );
 
-        client.content().delete( storeKey, path );
+        stream = new ByteArrayInputStream( content.getBytes() );
+        client.content().store( storeKey, path, stream );
 
-        assertThat( client.content().exists( storeKey, path ), equalTo( false ) );
+        assertThat( client.content().exists( storeKey, path ), equalTo( true ) );
 
+        final InputStream is = client.content().get( storeKey, path );
+        final String result = IOUtils.toString( is );
+
+        assertThat( result, equalTo( content ) );
+
+        is.close();
         stream.close();
     }
 
