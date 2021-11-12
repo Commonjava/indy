7d786de8fc61b87f2be9347326380f9741de2e6d
Wenjie Guo <wguo@redhat.com>
2021-11-12 09:50:59 +0800

    Fix the url mapping for slashTolerationFilter

--- subsys/jaxrs/src/main/java/org/commonjava/indy/bind/jaxrs/IndyDeployment.java
+++ subsys/jaxrs/src/main/java/org/commonjava/indy/bind/jaxrs/IndyDeployment.java
@@ -205,7 +205,7 @@ public class IndyDeployment
 
                                                       .addFilter( slashTolerationFilter )
                                                       .addFilterUrlMapping( slashTolerationFilter.getName(),
-                                                                            "//*", DispatcherType.REQUEST )
+                                                                            "/api/*", DispatcherType.REQUEST )
 
                                                       .addServlet( resteasyServlet )
 
