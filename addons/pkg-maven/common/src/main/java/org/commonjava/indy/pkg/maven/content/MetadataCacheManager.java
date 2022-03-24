b4de345429aa9000dbd12d20ccb4f3fe3fa582d0
Rui Han <ruhan@redhat.com>
2022-03-24 10:03:03 +0800

    Add config to disable maven metadata cache (#2049)
    * Add config to disable maven metadata cache
    
    * Remove mavenMetadataKeyCache

--- addons/pkg-maven/common/src/main/java/org/commonjava/indy/pkg/maven/content/MetadataCacheManager.java
+++ addons/pkg-maven/common/src/main/java/org/commonjava/indy/pkg/maven/content/MetadataCacheManager.java
@@ -15,25 +15,16 @@
  */
 package org.commonjava.indy.pkg.maven.content;
 
+import org.commonjava.indy.conf.InternalFeatureConfig;
 import org.commonjava.indy.model.core.StoreKey;
 import org.commonjava.indy.pkg.maven.content.cache.MavenMetadataCache;
-import org.commonjava.indy.pkg.maven.content.cache.MavenMetadataKeyCache;
 import org.commonjava.indy.subsys.infinispan.BasicCacheHandle;
 import org.commonjava.indy.subsys.infinispan.CacheHandle;
-import org.infinispan.Cache;
-import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.query.Search;
-import org.infinispan.query.dsl.Query;
-import org.infinispan.query.dsl.QueryFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class MetadataCacheManager
@@ -45,90 +36,44 @@ public class MetadataCacheManager
     private BasicCacheHandle<MetadataKey, MetadataInfo> metadataCache;
 
     @Inject
-    @MavenMetadataKeyCache
-    private BasicCacheHandle<MetadataKey, MetadataKey> metadataKeyCache;
-
-    private QueryFactory queryFactory;
+    private InternalFeatureConfig internalFeatureConfig;
 
     public MetadataCacheManager()
     {
     }
 
-    @PostConstruct
-    private void init()
-    {
-        if ( metadataKeyCache.getCache() instanceof RemoteCache )
-        {
-            this.queryFactory = org.infinispan.client.hotrod.Search.getQueryFactory(
-                            (RemoteCache) metadataKeyCache.getCache() );
-        }
-        else
-        {
-            this.queryFactory = Search.getQueryFactory( (Cache) metadataKeyCache.getCache() );
-        }
-    }
-
-    public MetadataCacheManager( CacheHandle<MetadataKey, MetadataInfo> metadataCache,
-                                 CacheHandle<MetadataKey, MetadataKey> metadataKeyCache )
+    public MetadataCacheManager( CacheHandle<MetadataKey, MetadataInfo> metadataCache, InternalFeatureConfig internalFeatureConfig )
     {
         this.metadataCache = metadataCache;
-        this.metadataKeyCache = metadataKeyCache;
-        this.queryFactory = Search.getQueryFactory( metadataKeyCache.getCache() );
+        this.internalFeatureConfig = internalFeatureConfig;
     }
 
     public void put( MetadataKey metadataKey, MetadataInfo metadataInfo )
     {
-        metadataKeyCache.put( metadataKey, metadataKey );
-        metadataCache.put( metadataKey, metadataInfo );
+        if (internalFeatureConfig.isMavenMetadataCacheEnabled()) {
+            metadataCache.put(metadataKey, metadataInfo);
+        }
     }
 
     public MetadataInfo get( MetadataKey metadataKey )
     {
-        return metadataCache.get( metadataKey );
+        if (internalFeatureConfig.isMavenMetadataCacheEnabled()) {
+            return metadataCache.get(metadataKey);
+        }
+        return null;
     }
 
     public void remove( StoreKey storeKey, String path )
     {
-        remove( new MetadataKey( storeKey, path ) );
-    }
-
-    public void remove( StoreKey key, Set<String> paths )
-    {
-        paths.forEach( p -> remove( new MetadataKey( key, p ) ) );
+        if (internalFeatureConfig.isMavenMetadataCacheEnabled()) {
+            remove(new MetadataKey(storeKey, path));
+        }
     }
 
     public void remove( MetadataKey metadataKey )
     {
-        metadataKeyCache.remove( metadataKey );
-        metadataCache.remove( metadataKey );
+        if (internalFeatureConfig.isMavenMetadataCacheEnabled()) {
+            metadataCache.remove(metadataKey);
+        }
     }
-
-    public void removeAll( StoreKey key )
-    {
-        getMatches( key ).forEach( k -> remove( k ) );
-    }
-
-    public Set<String> getAllPaths( StoreKey key )
-    {
-        return getMatches( key ).stream().map( ( k ) -> k.getPath() ).collect( Collectors.toSet() );
-    }
-
-    private List<MetadataKey> getMatches( StoreKey key )
-    {
-        Query query = queryFactory.from( MetadataKey.class )
-                                  .having( "storeKey.packageType" )
-                                  .eq( key.getPackageType() )
-                                  .and()
-                                  .having( "storeKey.type" )
-                                  .eq( key.getType().toString() )
-                                  .and()
-                                  .having( "storeKey.name" )
-                                  .eq( key.getName() )
-                                  .build();
-        List<MetadataKey> matches = query.list();
-
-        logger.debug( "Query metadataKeyCache for storeKey: {}, size: {}", key, matches.size() );
-        return matches;
-    }
-
 }
