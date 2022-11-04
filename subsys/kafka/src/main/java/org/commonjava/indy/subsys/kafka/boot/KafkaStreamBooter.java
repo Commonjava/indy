04cabec36b48c9574e02041d6eb891eab51b7e4c
Gang Li <ligangty@users.noreply.github.com>
2022-11-04 17:20:45 +0800

    Fix a potential closable leaking of kafka stream

--- subsys/kafka/src/main/java/org/commonjava/indy/subsys/kafka/boot/KafkaStreamBooter.java
+++ subsys/kafka/src/main/java/org/commonjava/indy/subsys/kafka/boot/KafkaStreamBooter.java
@@ -71,8 +71,8 @@ public class KafkaStreamBooter
             serviceEventHandler.dispatchEvent( stream, topic );
         }
         Properties props = setKafkaProps();
-        final KafkaStreams streams = new KafkaStreams( builder.build(), props );
-        try
+
+        try (final KafkaStreams streams = new KafkaStreams( builder.build(), props ))
         {
             streams.start();
         }
