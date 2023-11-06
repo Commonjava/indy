afa8882ad5aa88cc557c6bbd6875af2505ac274a
Gang Li <ligangty@users.noreply.github.com>
2023-11-06 21:17:05 +0800

    Revert the content-browse back

--- addons/content-browse/ui/webpack.dev.config.js
+++ addons/content-browse/ui/webpack.dev.config.js
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/main/js/app.js',
