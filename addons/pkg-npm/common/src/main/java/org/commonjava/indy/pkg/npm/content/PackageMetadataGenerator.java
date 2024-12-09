e21e661fb6985cd289ddf767955901e41bc4c317
liyu <liyu@redhat.com>
2024-12-09 18:33:34 +0800

    check tarball string as well in the generation phase

--- addons/pkg-npm/common/src/main/java/org/commonjava/indy/pkg/npm/content/PackageMetadataGenerator.java
+++ addons/pkg-npm/common/src/main/java/org/commonjava/indy/pkg/npm/content/PackageMetadataGenerator.java
@@ -321,9 +321,10 @@ public class PackageMetadataGenerator
                 }
 
                 // Generate tarball url if missing
-                if ( versionMetadata.getDist() == null )
+                String tarball = "http://indy/" + packagePath.getTarPath(); // here we use mock host. indy will amend it with the right hostname
+ 
+                if ( versionMetadata.getDist() == null || !versionMetadata.getDist().getTarball().equals(tarball) )
                 {
-                    String tarball = "http://indy/" + packagePath.getTarPath(); // here we use mock host. indy will amend it with the right hostname
                     //logger.debug( "Generate dist tarball: {}", tarball );
                     versionMetadata.setDist( new Dist(  tarball ) );
                 }
